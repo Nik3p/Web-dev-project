86d089c9f57b2ba20967098f77acb6b1304b68b1
gianniskallionis <gianniskallionis@gmail.com>
2026-04-30 17:37:11 +0300

    working on flask

--- app.py
+++ app.py
@@ -1,3 +1,37 @@
-from flask import flask
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+import numpy as np
 
-app=flask(__name__)
+app=Flask(__name__)
+CORS(app)
+
+ # connecting database with flask 
+app.config["MONGO_URI"]="mongodb://localhost:27017/netflex"
+mongo= PyMongo(app)
+
+
+@app.route ('/like',methods=['POST']) # tha to kanei o nikos 
+def like():
+   data.request
+
+
+
+
+@app.route('/search')     # tha to kanei o giannis 
+def search():
+  mongo.db.netflex.find({"$search":"value"}       ).list().
+
+
+@app.route('/popular') # tha to kanei o giannis 
+def popular():
+   
+
+
+
+
+
+
+
+if __name__=='__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
