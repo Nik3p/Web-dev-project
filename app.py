91d73c9e43b6ee4f0f3a49b457f306ebc65826a2
nik <nik@debian.home>
2026-05-01 18:01:52 +0300

    added /like endpoint

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 import numpy as np
+from bson.objectid import ObjectId
 
 app=Flask(__name__)
 CORS(app)
@@ -11,18 +12,23 @@ app.config["MONGO_URI"]="mongodb://localhost:27017/netflex"
 mongo= PyMongo(app)
 
 
-@app.route ('/like',methods=['POST']) # tha to kanei o nikos 
+@app.route ('/like',methods=['POST'])
 def like():
-   data.request
+   data = request.get_json()
+   id = data["id"]
+   mongo.db.athletes.update_one({"_id": Obje ctId(id)},{"$inc": {"likes": 1}})
+   return jsonify({"message": "Liked!"})
+
+   
 
 
 
 
-@app.route('/search')     # tha to kanei o giannis 
+@app.route('/search')    
 def search():
-  nameSearched=request.args.get("name","") 
+   nameSearched=request.args.get("name","") 
   # an den valei name, epistrefei empty string, dld to ""
-  return jsonify(list(mongo.db.athletes.find({"name": {"$regex":nameSearched,"$options":"i"}}).sort("price",-1)))
+   return jsonify(list(mongo.db.athletes.find({"name": {"$regex":nameSearched,"$options":"i"}}).sort("price",-1)))
 # to $regex psaxnei na vrei an mia akolouthia p exei grapsei o allos yparxei se kapoio name
 # an grapseis gi sou vriskei to giannis ( gi is in giannis)
 #to "$options": "i" . to option shmainei rythmiseis kai to i shmainei case Insensitive
@@ -32,7 +38,7 @@ def search():
 
  # /popular: Αναπτύξτε ένα GET request που θα επιστρέφει μία λίστα με τα top-5 πιο
 #δημοφιλή προϊόντα με βάση των αριθμό των Likes.
-@app.route('/popular') # tha to kanei o giannis 
+@app.route('/popular') 
 def get5MostPopular():
    return jsonify(list(mongo.db.athletes.find().sort("likes",-1).limit(5)))
    # to find ta fernei ola, to sort likes-1 krataei ta top me likes apo max pros min
